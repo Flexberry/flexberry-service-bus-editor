03f7c6abc35333b396eb468a08920347b4bb4928
DubrovinPavel <dubrovip@yandex.ru>
2018-02-20 13:27:03 +0500

    Add inflector rules

---
+++ app/models/custom-inflector-rules.js
@@ -0,0 +1,8 @@
+import Inflector from 'ember-inflector';
+
+const inflector = Inflector.inflector;
+
+inflector.irregular('entity', 'Entitys');
+inflector.irregular('access', 'Accesss');
+
+export default {};
